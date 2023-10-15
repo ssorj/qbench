89e65e095cffa460b0c43c932161cd176151d189
Justin Ross <jross@apache.org>
2023-10-15 11:46:48 -0400

    WIP

--- python/qbench/main.py
+++ python/qbench/main.py
@@ -95,7 +95,7 @@ class Runner:
                 with ProcessMonitor(server_proc.pid) as server_mon:
                     if self.config.duration:
                         sleep(self.config.duration)
-                    else: # pragma: no cover
+                    else: # pragma: nocover
                         while True:
                             sleep(86400)
 
@@ -130,7 +130,7 @@ class Runner:
         run("cat qbench.log.* > qbench.log", shell=True)
         run("rm qbench.log.*", shell=True)
 
-        if get_file_size("qbench.log") == 0: # pragma: no cover
+        if get_file_size("qbench.log") == 0: # pragma: nocover
             raise Exception("No data in logs")
 
         data = _pandas.read_csv("qbench.log", header=None, dtype="int")
